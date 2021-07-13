dd8ffb40c74bd1f561ac3272c2599ac2aef645d4
LizhiyuanLin <lizhiyuanlin@gmail.com>
2021-07-13 20:31:53 +0000

    ORM implemented

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField()
+    description = models.CharField()
+
+    def __str__(self):
+        return f"Make: {self.name}, Description: {self.description}"
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,7 +25,23 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    make = models.ForeignKey(CarMake)
+    name = models.CharField()
+    dealer_id = models.IntegerField()
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon')
+    ]
+    car_type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES)
+    year = models.DateField()
 
+    def __str__(self):
+        return f"Make: {self.make}, Name: {self.name}, Dealer Id: {self.dealer_id}, Type: {self.car_type}, Year: {self.year}"
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
