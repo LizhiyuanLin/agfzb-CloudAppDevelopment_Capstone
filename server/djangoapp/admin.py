0e034f7960988a068f495ce5b7ecdfc4ff6a69e4
LizhiyuanLin <lizhiyuanlin@gmail.com>
2021-07-13 20:54:14 +0000

    ORM implemented

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarModel, CarMake
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 1
 # CarModelAdmin class
-
+class CarModelAdmin(admin.StackedInline):
+    list_display = ['title']
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.StackedInline):
+    inlines = [CarModelInline]
+    list_display = ('name', 'description')
+    list_filter = ['name']
+    search_fields = ['name']
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
