2e087f059e0d8315094917e0fa37061db1bb39aa
LizhiyuanLin <lizhiyuanlin@gmail.com>
2021-07-16 19:06:17 +0000

    datepicker issue

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -71,7 +71,7 @@ class CarDealer:
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
 class DealerReview:
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year):
+    def __init__(self, dealership=None, name=None, purchase=None, review=None, purchase_date=None, car_make=None, car_model=None, car_year=None):
         self.dealership = dealership
         self.name = name
         self.purchase = purchase
